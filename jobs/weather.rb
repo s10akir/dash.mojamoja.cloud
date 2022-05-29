2cdbb1f2b8c8d989eae5b718412fc929dd21b17f
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-05-29 13:28:32 +0000

    feat: create widget and job 'weather'

---
+++ jobs/weather.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+BASE_URL = 'https://weather.tsukumijima.net/api/forecast'
+AREA_CODE_YOKOHAMA = 140010
+
+conn = Faraday.new(url: BASE_URL)
+
+SCHEDULER.every '1d', :first_in => 0 do |job|
+  data = nil
+
+  res = JSON.parse(conn.get('/api/forecast', { city: AREA_CODE_YOKOHAMA }).body, symbolize_names: true)
+  forecasts = res[:forecasts]
+
+  send_event('weather_today', data: forecasts[0])
+  send_event('weather_tomorrow', data: forecasts[1])
+end
