8023e0e2b90cad14d29f479477b9057ac833124d
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-05-29 16:07:50 +0000

    feat: create TextSimple widget

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -10,7 +10,9 @@ SCHEDULER.every '1d', :first_in => 0 do |job|
 
   res = JSON.parse(conn.get('/api/forecast', { city: AREA_CODE_YOKOHAMA }).body, symbolize_names: true)
   forecasts = res[:forecasts]
+  description = res[:description][:text]
 
   send_event('weather_today', data: forecasts[0])
   send_event('weather_tomorrow', data: forecasts[1])
+  send_event('weather_description', data: description)
 end
---
+++ widgets/text_simple/text_simple.coffee
@@ -0,0 +1,1 @@
+class Dashing.TextSimple extends Dashing.Widget
